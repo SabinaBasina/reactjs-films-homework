16444a110f4ae0383a07d53e2d93dcb2bc972ee2
SabinaBasina <sabina98bel@mail.ru>
2019-04-02 19:04:36 +0300

    part2 ( update )

--- src/components/Signature/__tests__/Signature.test.jsx
+++ src/components/Signature/__tests__/Signature.test.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
+import TestRenderer from 'react-test-renderer';
+import { BrowserRouter as MemoryRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Signature from '../Signature';
+import store from '../../../modules/store';
+// import TvShowDetails from '../../../pages/TvShowDetails';
 
-jest.mock(
-  '../../TvShowDetails/TvShowDetails',
-  () => id => `mock data${JSON.stringify(id)}`,
-);
 
-test('Signature test', () => {
-  const renderer = new ShallowRenderer();
-  renderer.render(<Signature />);
-  const result = renderer.getRenderOutput();
-
-  expect(result).toMatchSnapshot();
+test('renders', () => {
+  const component = TestRenderer.create(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/tvShowDetails/:id']} initialIndex={1}>
+        <Signature />
+        <Route
+          path="/tvShowDetails/:id"
+          render={({ match }) => <div>{match.params.id}</div>}
+        />
+      </MemoryRouter>
+    </Provider>,
+  );
+  expect(component).toMatchSnapshot();
 });
