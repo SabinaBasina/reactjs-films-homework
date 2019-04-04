7b75f6972bded94e8a12b52788eedd0621032bd4
SabinaBasina <sabina98bel@mail.ru>
2019-04-04 16:27:39 +0300

    part1 ( update )

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Signature from '../components/Signature/Signature';
+import Signature from '../Signature';
 
 const App = () => (
   <Signature name="Sabina Basina" />
